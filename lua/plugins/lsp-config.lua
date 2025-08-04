85215e1095b0afd9ceeeb83b58b9a98e33d25a02
Minetriforce <jonathand.ngo.65@gmail.com>
2025-08-04 14:42:51 +0200

    Ajout des LSP, lua, python, java + qq raccourcis claviers à revérifier

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,72 @@
+return {
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = { "lua_ls", "pyright", "marksman", "jdtls" }
+            })
+        end
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local lspconfig = require("lspconfig")
+            lspconfig.lua_ls.setup({})
+
+            -- Configuration pour Python
+            lspconfig.pyright.setup({})
+
+            -- Configuration pour Markdown
+            lspconfig.marksman.setup({})
+
+            -- Configuration pour Java (jdtls)
+            lspconfig.jdtls.setup({
+                cmd = {
+                    'jdtls',
+                    '--jvm-arg=-javaagent:' .. vim.fn.expand('~/.local/share/nvim/mason/packages/jdtls/lombok.jar'),
+                },
+                root_dir = function(fname)
+                    return lspconfig.util.root_pattern('pom.xml', 'gradle.build', '.git')(fname) or vim.fn.getcwd()
+                end,
+                settings = {
+                    java = {
+                        eclipse = {
+                            downloadSources = true,
+                        },
+                        configuration = {
+                            updateBuildConfiguration = "interactive",
+                        },
+                        maven = {
+                            downloadSources = true,
+                        },
+                        implementationsCodeLens = {
+                            enabled = true,
+                        },
+                        referencesCodeLens = {
+                            enabled = true,
+                        },
+                        references = {
+                            includeDecompiledSources = true,
+                        },
+                    }
+                },
+                init_options = {
+                    bundles = {}
+                },
+            })
+
+            vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
+            -- vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { desc = 'Go to definition' })
+            vim.keymap.set('n', 'gr', vim.lsp.buf.references, { desc = 'Go to references' })
+            vim.keymap.set('n', 'K', vim.lsp.buf.hover, { desc = 'Hover documentation' })
+            vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { desc = 'Rename symbol' })
+            vim.keymap.set('n', '<C-M-l>', vim.lsp.buf.format, { desc = 'Format code' })
+        end
+    }
+}
